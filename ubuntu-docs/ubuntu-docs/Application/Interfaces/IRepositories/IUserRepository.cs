fa35d23c1f6525ae488aaa157e13ba05317c750d
SfisoNxumalo <sfisonxumalo69@gmail.com>
2026-03-27 23:59:50 +0200

    Add user repository and interface

---
+++ ubuntu-docs/ubuntu-docs/Application/Interfaces/IRepositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using ubuntu_docs.Domain.Entities;
+
+namespace ubuntu_docs.Application.Interfaces.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity> CreateAsync(UserEntity user);
+
+        Task<UserEntity?> GetByIdAsync(Guid id);
+
+        Task<UserEntity?> GetByEmailAsync(string email);
+
+        Task<IEnumerable<UserEntity>> GetAllAsync();
+
+        Task UpdateAsync(UserEntity user);
+
+        Task DeleteAsync(Guid id);
+    }
+}
